d5c228ad63e657f47a97c24a50a2a1cf39d1d44a
Eduard <eduard.kamburjan@gmx.de>
2021-02-22 18:25:41 +0100

    Minor refactoring

--- src/main/kotlin/microobject/type/Types.kt
+++ src/main/kotlin/microobject/type/Types.kt
@@ -5,30 +5,11 @@ abstract class Type {
     abstract fun isFullyConcrete() : Boolean
     abstract fun getPrimary() : SimpleType
     abstract fun containsUnknown(types: Set<String>): Boolean
-    fun isAssignable(rhs : Type, extends : MutableMap<String, String>) : Boolean { //TODO: finish refactoring
+    open fun isAssignable(rhs : Type, extends : MutableMap<String, String>) : Boolean {
         if (this == ERRORTYPE || rhs == ERRORTYPE) return false   //errors are not assignable
         if (rhs == NULLTYPE) return true
         if (this == rhs) return true  //no need for complex typing
-        if (this is BaseType && rhs is BaseType) {
-            return rhs.isBelow(this, extends)
-        } else if (this is GenericType && rhs is GenericType) {
-            return this == rhs
-        } else if (this is SimulatorType && rhs is SimulatorType) {
-            return rhs.inVar.containsAll(this.inVar) && rhs.outVar.containsAll(this.outVar)
-        } else if (this.javaClass != rhs.javaClass) {
-            return false
-        }  else { //if (lhs is ComposedType && rhs is ComposedType)
-            val cLhs = this as ComposedType
-            val cRhs = rhs as ComposedType
-            if(cLhs.params.size != cRhs.params.size) return false
-
-            var ret = cLhs.name.isAssignable(cRhs.name, extends)
-            for( i in cLhs.params.indices){
-                if(!ret) break
-                ret = ret && cLhs.params[i].isAssignable(cRhs.params[i], extends)
-            }
-            return ret
-        }
+        return false
     }
     fun isBelow(t2: Type, extends : MutableMap<String, String>) : Boolean {
         if(this == t2) return true
@@ -51,20 +32,24 @@ data class SimulatorType(val inVar : List<Pair<String, Type>>, val outVar : List
         inVar.map { it.second }.any { it.isFullyConcrete() } && outVar.map { it.second }.any { it.isFullyConcrete() }
     override fun containsUnknown(types: Set<String>): Boolean =
         inVar.map { it.second }.any { it.containsUnknown(types) } || outVar.map { it.second }.any { it.containsUnknown(types) }
+    override fun isAssignable(rhs : Type, extends : MutableMap<String, String>) : Boolean  =
+        super.isAssignable(rhs, extends) || (rhs is SimulatorType && rhs.inVar.containsAll(this.inVar) && rhs.outVar.containsAll(this.outVar))
 }
 
 data class GenericType(val name : String) : SimpleType() {
-    override fun getNameString() : String  = name
-    override fun toString() : String  = name
-    override fun isFullyConcrete() : Boolean = false
+    override fun getNameString(): String = name
+    override fun toString(): String = name
+    override fun isFullyConcrete(): Boolean = false
     override fun containsUnknown(types: Set<String>): Boolean = !types.contains(name)
+    override fun isAssignable(rhs: Type, extends: MutableMap<String, String>): Boolean = super.isAssignable(rhs, extends)
 }
-
 data class BaseType(val name : String) : SimpleType(){
     override fun getNameString() : String  = name
     override fun isFullyConcrete() : Boolean = true
     override fun toString() : String  = name
     override fun containsUnknown(types: Set<String>): Boolean = false //contract
+    override fun isAssignable(rhs : Type, extends : MutableMap<String, String>) : Boolean =
+        super.isAssignable(rhs, extends) || (rhs is BaseType && rhs.isBelow(this, extends))
 }
 
 data class ComposedType(val name : Type, val params : List<Type>) : Type() {
@@ -75,7 +60,20 @@ data class ComposedType(val name : Type, val params : List<Type>) : Type() {
     override fun toString() : String  = name.toString() + "<" + params.joinToString { it.toString() }+">"
     override fun isFullyConcrete() : Boolean = params.fold(name.isFullyConcrete(), { it,nx -> it && nx.isFullyConcrete()})
     override fun containsUnknown(types: Set<String>): Boolean = params.fold( name.containsUnknown(types), {acc,nx -> acc || nx.containsUnknown(types)})
+    override fun isAssignable(rhs : Type, extends : MutableMap<String, String>) : Boolean {
+        if(super.isAssignable(rhs, extends)) return true
+        if(rhs !is ComposedType) return false
+        if (this.params.size != rhs.params.size) return false
+
+        var ret = this.name.isAssignable(rhs.name, extends)
+        for (i in this.params.indices) {
+            if (!ret) break
+            ret = ret && this.params[i].isAssignable(rhs.params[i], extends)
+        }
+        return ret
+    }
 }
+
 
 val INTTYPE = BaseType("Int")
 val BOOLEANTYPE = BaseType("Boolean")
