b03a3c491bc281538e705a6a3a998486de0ef18f
Riccardo Sieve <sievericcardo@gmail.com>
2024-02-16 16:23:03 +0100

    Update ReclassifyStmt.kt
    changed method

--- src/main/kotlin/no/uio/microobject/ast/stmt/ReclassifyStmt.kt
+++ src/main/kotlin/no/uio/microobject/ast/stmt/ReclassifyStmt.kt
@@ -1,12 +1,13 @@
 package no.uio.microobject.ast.stmt
 
 import no.uio.microobject.ast.Statement
+import no.uio.microobject.ast.expr.LiteralExpr
 import no.uio.microobject.runtime.EvalResult
 import no.uio.microobject.runtime.Interpreter
 import no.uio.microobject.runtime.Memory
 import no.uio.microobject.runtime.StackEntry
 
-data class ReclassifyStmt(val className: String, val staticTable: MutableMap<String, String>) : Statement {
+data class ReclassifyStmt(val oldState: LiteralExpr, val className: String, val staticTable: MutableMap<String, String>) : Statement {
     override fun toString(): String = "Reclassify to a $className"
 
     override fun getRDF(): String {
@@ -17,19 +18,17 @@ data class ReclassifyStmt(val className: String, val staticTable: MutableMap<Str
         for ((key, value) in staticTable) {
             // Check if key is a subclass of className
             if (isSubclassOf(key, className)) {
-                // Perform the query associated with key
-                val results = interpreter.query(value.removePrefix("\"").removeSuffix("\""))
                 val queryResult = interpreter.query(value)
 
                 // If the query returns true, return the class associated with key
                 if (queryResult != null) {
-                    return EvalResult(key)
+                    interpreter.reclassify(oldState, key);
                 }
             }
         }
 
         // If no subclass of className is found, return className
-        return EvalResult(className)
+        return EvalResult(stackFrame, listOf())
     }
 
     private fun isSubclassOf(subclass: String, superclass: String): Boolean {
